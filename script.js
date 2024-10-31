a372c0dddb7bedce2b7d93e62b220e8abb472301
newopik <guebejobanget@gmail.com>
2024-10-31 08:53:25 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
-function tampilkanPesan() {
-  alert("Halo! Anda menekan tombol.");
-}
+// Tambahkan event listener untuk menampilkan pesan di konsol saat menu diklik
+document.addEventListener("DOMContentLoaded", function() {
+    const links = document.querySelectorAll("nav ul li a");
+    links.forEach(link => {
+        link.addEventListener("click", function() {
+            console.log(`Navigasi ke ${link.textContent}`);
+        });
+    });
+});
